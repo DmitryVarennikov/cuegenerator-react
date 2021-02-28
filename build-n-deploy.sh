5e6cacd86eefd7cec3f3fa5a881b5ea82d578ed0
Dmitry Varennikov <dmitry.varennikov@gmail.com>
2021-02-28 09:24:43 -0500

    Init

---
+++ build-n-deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+npm run build
+rm -rf public-prod
+mv build public-prod
+npm run deploy
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+// import CGLog from '../public/images/CUEgenerator.png';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="images/CUEgenerator.png" className="App-logo" alt="logo" />
+        <p>powered by React</p>
+        <p>provisioned with custom `build-n-deploiy.sh` script</p>
       </header>
     </div>
   );
