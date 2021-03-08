08d7bebbad2ea32c1acb546052a18e1feba05955
Dmitry Varennikov <dmitry.varennikov@gmail.com>
2021-03-07 19:44:49 -0500

    Close #6: Parse, format cue input and display it in the read-only text area

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 import Form from './Components/Form';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div id="container">
-      <h1
-        id="logo"
-        style={{ backgroundImage: `url("/images/CUEgenerator.png")` }}
-      >
-        <a href=".">CUEgenerator</a>
-      </h1>
-      <span id="counter">(123)</span>
-
-      <div id="feedback">
-        <div className="links">
-          <a
-            href="https://github.com/dVaffection/cuegenerator-react"
-            target="_blank" rel="noreferrer"
-          >
-            Leave your feedback on GitHub
-          </a>
-        </div>
-      </div>
-
-      <div className="clear"></div>
-
+      <Header />
       <Form />
     </div>
   );
